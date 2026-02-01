8d9ca92df49447310d86647ec6150b62511b30ea
JeswinAaronG <jeswinaaron2005@gmail.com>
2026-02-01 23:06:16 +0530

    Add files via upload

---
+++ Plus_one_leet.py
@@ -0,0 +1,12 @@
+digits = [9]
+n = len(digits)
+
+def plus_one(digits,n):
+    for i in range(n-1,-1,-1):
+        if digits[i] < 9:
+            digits[i] += 1
+            return digits
+        digits[i] = 0
+    return [1]+digits
+
+print(plus_one(digits,n))
